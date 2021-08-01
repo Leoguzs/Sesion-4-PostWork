ac57014a728ee0780db06035f67514ed65736a56
Itzama <fernandonovca@gmail.com>
2021-07-31 19:01:40 -0500

    added ejercicio 3

--- index.js
+++ index.js
@@ -2,35 +2,35 @@
 son objetos con las mismas propiedades, en este último caso los valores de las propiedades deben 
 ser comparados con una llamada recursiva de deepEqual.*/
 function deepEqual(a, b) {
-//código
+  //código
 }
-    
+
 
 //Objetos a comparar en deepEqual
-  var john = {
-    firstName: 'John',
-    lastName: 'Doe'
-  }
-  
-  var jane = {
-    firstName: 'Jane',
-    lastName: 'Doe'
-  }
+var john = {
+  firstName: 'John',
+  lastName: 'Doe'
+}
 
-  var luca = {
-      firstName: 'Luca'
-  }
+var jane = {
+  firstName: 'Jane',
+  lastName: 'Doe'
+}
+
+var luca = {
+  firstName: 'Luca'
+}
 
 
 /*Ej:2 función chunk que recibe un arreglo y un número entero size. La 
 función debe dividir el arreglo en múltiples arreglos del tamaño determinado por size.*/
 function chunk(array, size) {
-    const newArr = [];
+  const newArr = [];
 
-    for(let i = 0; i < array.length / size; ++i)
-        newArr.push(array.slice(i*size, i*size + size));
+  for (let i = 0; i < array.length / size; ++i)
+    newArr.push(array.slice(i * size, i * size + size));
 
-     return newArr;
+  return newArr;
 };
 //Arreglo para test en chunk
 const data = [1, 2, 3, 4, 5, 6, 7, 8];
@@ -50,22 +50,35 @@ console.log('Test 3:', chunk(data, 3)) // [[1, 2, 3], [4, 5, 6], [7, 8]]
 //   chunk(oneMoreData, 2)
 
 
-/*Ej:3 Escribir una función frequency que recibe un string como argumento.*/
-  function frequency(string) {
- if (typeof string != 'string') {
-    throw TypeError ('El argumento debe de ser una cadena de caracteres')
-    } 
-let contador= {}; //literal de diccionario de mapa vacío
-string.replace(/\S/g, function(w) { //word 
-    contador[w] = (isNaN(contador[w]) ? 1 : contador [w] + 1);
-});
-return contador;
 
+/* EJERCICIO 3
+
+Escribir una función frequency que recibe un string como argumento. 
+Esta función debe contar la frecuencia o el número de veces que se repite cada carácter. 
+El resultado debe mostrarse en un objeto donde las propiedades sean los caracteres, y los valores sean la frecuencia. 
+Los resultados deben ordenarse ascendentemente por los caracteres y no la frecuencia.
+*/
+
+
+
+function frequency(string) {
+  const charArray = string.split("").sort();
+  let freqObject = {};
+  for (char of charArray) {
+    if (freqObject[char]) {
+      freqObject[char]++;
+    } else {
+      freqObject[char] = 1;
+    }
   }
+  return freqObject;
+};
 
-  try {
-      console.log(frequency('Leonidas'));
-  } catch (e) {
-    console.log(`Error ${e.message}`)
-      
-  } 
+
+
+console.log('Test 1:', frequency('cccbbbaaa'))
+// {a: 3, b: 3, c: 3}
+console.log('Test 2:', frequency('www.bedu.org'))
+// {.: 2, b: 1, d: 1, e: 1, g: 1, o: 1, r: 1, u: 1, w: 3}
+console.log('Test 3:', frequency('john.doe@domain.com'))
+   // {.: 2, @: 1, a: 1, c: 1, d: 2, e: 1, h: 1, i: 1, j: 1, m: 2, n: 2, o: 4}
